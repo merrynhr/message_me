7f3520a82f0988b963fe6860dc2b0061bb52d739
Merryn <merrynhr@gmail.com>
2021-10-11 20:38:10 +0100

    set up jquery and popper.js for rails 6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,23 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require semantic-ui
 
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
+import 'jquery'
+
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
