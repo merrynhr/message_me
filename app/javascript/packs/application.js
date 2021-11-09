d988d66809e48881d9313cc707960cfa64eee862
Merryn <merrynhr@gmail.com>
2021-11-09 15:55:23 +0000

    added scroll_bottom function to application.js file for auto scrolling in chatbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,11 +12,18 @@ import '@doabit/semantic-ui-sass'
 import 'jquery'
 
 
+window.scroll_bottom = function(){
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function(){
     $(this).closest('.message').transition('fade');
   });
+  scroll_bottom();
 });
 
 
